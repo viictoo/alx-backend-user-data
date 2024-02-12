3c48d584b4986ba0205569eb51fce6235497b39f
Victor Langat <provicml@gmail.com>
2024-02-12 20:25:25 +0300

    add not require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,18 +5,26 @@ from flask import jsonify, abort, request
 from api.v1.views import app_views
 from typing import TypeVar, List
 
+
 class Auth:
     """class to manage the API authentication"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns False"""
-        return False
+        if not path or not excluded_paths or excluded_paths == []:
+            return True
+        if not path.endswith('/'):
+            path = path + '/'
 
-    def authorization_header(self, request=None) -> str: 
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
         """returns None"""
 
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None"""
 
