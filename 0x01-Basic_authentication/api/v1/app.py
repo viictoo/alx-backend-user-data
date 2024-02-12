2747336df7e6ad9c61e0fe9c92aec32a100b69c9
Victor Langat <provicml@gmail.com>
2024-02-13 00:22:07 +0300

    test fix requiremnts

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,8 +7,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import  BasicAuth
 
 
 app = Flask(__name__)
@@ -18,8 +16,10 @@ auth = None
 auth = getenv("AUTH_TYPE")
 
 if auth == "basic_auth":
+    from api.v1.auth.basic_auth import  BasicAuth
     auth = BasicAuth()
 else:
+    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
@@ -60,4 +60,4 @@ def forbidden(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
