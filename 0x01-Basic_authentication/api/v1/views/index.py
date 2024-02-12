ac15ec1db51d2c0eaf6b3c17d08ffd6032591840
Victor Langat <provicml@gmail.com>
2024-02-12 19:55:59 +0300

    add 401 unathorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def error_401() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    abort(401)
