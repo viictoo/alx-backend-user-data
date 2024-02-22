7d78d2d5181a71d948c9fd16948cf7807eb19b58
Victor Langat <provicml@gmail.com>
2024-02-22 10:57:56 +0300

    add code: create user session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """user authentication module"""
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from uuid import uuid4
 
 from sqlalchemy.orm.exc import NoResultFound
@@ -42,3 +42,55 @@ class Auth:
             return self._db.add_user(email, _hash_password(password))
         else:
             raise ValueError(f'User {email} already exists')
+        
+    def valid_login(self, email: str, password: str) -> bool:
+        """registers a new user given a new password and email
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return checkpw(
+                password.encode("utf-8"),
+                user.hashed_password
+                )
+        except NoResultFound:
+            return False
+    
+    def create_session(self, email: str) -> str:
+        """registers a new user given a new password and email
+        """
+        try:
+            # ver1
+            # in memory storage: lost sessiionif server crashes
+            # user = self._db.find_user_by(email=email)
+            # user.session_id = _generate_uuid()
+            # return user.session_id
+
+            # ver 2
+            # imeediate persistent db storage: 
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id = session_id)
+            return user.session_id
+        
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, email: str) -> str:
+        """registers a new user given a new password and email
+        """
+        try:
+            # ver1
+            # in memory storage: lost sessiionif server crashes
+            # user = self._db.find_user_by(email=email)
+            # user.session_id = _generate_uuid()
+            # return user.session_id
+
+            # ver 2
+            # imeediate persistent db storage: 
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id = session_id)
+            return user.session_id
+        
+        except NoResultFound:
+            return None
